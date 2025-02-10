7b92122f99369f2399070bde4f8dd1d2e57ce2e3
Aidai Amanbekova <aidaiamanbekova8@gmail.com>
2025-02-10 12:51:41 +0300

    Doctors page , page appointment and new related doctors file

---
+++ src/components/RelatedDoctors.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext";
+
+const RelatedDoctors = ({speciality, docId}) =>{
+
+    const{doctors} = useContext(AppContext)
+
+    const {relDoc, setRelDocs} = useState([])
+
+    useEffect(()=>{
+        if (doctors.length > 0 && speciality){
+            const doctorsData = doctors.filter((doc)=> doc.speciality === speciality && doc._id !== docId)
+            setRelDocs(doctorsData)
+        }
+    },[doctors,speciality,docId])
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default RelatedDoctors
